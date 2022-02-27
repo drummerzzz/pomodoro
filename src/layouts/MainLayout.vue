b728d50790ead765ab156cd0c0b88e45e636a3f3
Drummerzzz <jf.interatividade@gmail.com>
2022-02-27 01:31:25 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header v-if="false" elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup () {
+    const $q = useQuasar()
+    $q.dark.set(true) // or false or "auto"
+  }
+})
+</script>
